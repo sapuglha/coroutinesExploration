6da9cc215211ad64cdcf1420b8c9598bbdae032e
Tiago Silva <sapuglha@gmail.com>
2019-02-17 22:23:22 -0700

    Consolidate UserDao and UserEntity into one file

--- app/src/main/java/com/sapuglha/coroutinesexploration/di/ViewModelModule.kt
+++ app/src/main/java/com/sapuglha/coroutinesexploration/di/ViewModelModule.kt
@@ -5,7 +5,6 @@ import com.sapuglha.coroutinesexploration.presentation.ViewModelFactory
 import dagger.Binds
 import dagger.Module
 
-
 /*
  * This needs to be in a separate module since you can't mix @Provides and @Binds in the same module
  */
