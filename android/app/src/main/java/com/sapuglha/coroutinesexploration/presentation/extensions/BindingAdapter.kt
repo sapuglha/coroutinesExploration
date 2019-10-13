7e3e2e13fd0a065cd819c38eab29045fd370fc9a
Tiago Silva <sapuglha@gmail.com>
2019-10-13 13:09:27 -0600

    Code cleanup

--- android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/extensions/BindingAdapter.kt
+++ android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/extensions/BindingAdapter.kt
@@ -5,11 +5,6 @@ import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("fieldValidationError")
 fun TextInputLayout.setErrorMessage(errorMessage: String?) {
-    if (errorMessage.isNullOrBlank()) {
-        this.error = ""
-        this.isErrorEnabled = false
-    } else {
-        this.error = errorMessage
-        this.isErrorEnabled = true
-    }
+    error = errorMessage ?: ""
+    isErrorEnabled = errorMessage?.isNotBlank() ?: false
 }
--- android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/extensions/Extensions.kt
+++ android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/extensions/Extensions.kt
@@ -7,15 +7,13 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-fun Fragment.hideKeyboard() {
+fun Fragment.hideKeyboard() =
     view?.let { activity?.hideKeyboard(it) }
-}
 
-fun AppCompatActivity.hideKeyboard() {
+fun AppCompatActivity.hideKeyboard() =
     hideKeyboard(if (currentFocus == null) View(this) else currentFocus)
-}
 
-fun Context.hideKeyboard(view: View) {
-    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-}
+fun Context.hideKeyboard(view: View) =
+    (getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+        hideSoftInputFromWindow(view.windowToken, 0)
+    }
