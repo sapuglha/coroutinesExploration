5d7497273a9bb41304c224e7756e5cacea178523
Tiago Silva <sapuglha@gmail.com>
2019-10-13 14:09:59 -0600

    Add bottom navigation option for Location fragment

--- android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/MainActivity.kt
+++ android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sapuglha.coroutinesexploration.R
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.navFragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.bottom_menu_item_users,
+                R.id.bottom_menu_item_location
+            )
+        )
 
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+
+        setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Setup the bottom navigation
+        bottomNavigationView.setupWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.navFragment).navigateUp()
 }
---
+++ android/app/src/main/java/com/sapuglha/coroutinesexploration/presentation/location/LocationFragment.kt
@@ -0,0 +1,7 @@
+package com.sapuglha.coroutinesexploration.presentation.location
+
+import androidx.fragment.app.Fragment
+
+class LocationFragment : Fragment() {
+
+}
